9f53b7832812ba6dee5636ed3eed65db89b27c9e
zoltanlederer <zoltanlederer@yahoo.com>
2020-09-16 19:36:24 +0200

    Initial update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// class CountingParent extends React.Component {
+//     state = {
+//       actionCount: 0,
+//     };
+
+//   handleAction = (action) => {
+//     console.log("Child says", action);
+//     this.setState(() => {
+//       return {
+//         actionCount: this.state.actionCount + 1,
+//       }
+//     });
+//   }
+//   resetAction = () => {
+//     this.setState(() => {
+//       return {
+//         actionCount: 0,
+//       }
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <Child onAction={this.handleAction} onActionR={this.resetAction} />
+//         <p>Clicked {this.state.actionCount} times</p>
+//       </div>
+//     );
+//   }
+// }
+
+// function Child({ onAction, onActionR }) {
+//   return (
+//     <div>
+//       <button onClick={onAction}>Click Me!</button>
+//       <button onClick={onActionR}>RESET ME!</button>
+//     </div>
+//   );
+// };
+
+
+/*
+Exercises
+1. Create a component called House to model a home with 4 rooms, each with its own light and
+lightswitch. Use state to keep track of whether each light is on or off. Add 4 buttons to represent
+the lightswitches, and flip the respective light on or off when the buttons are clicked. Use this
+initial state:
+*/
+
+class House extends React.Component {
+  state = {
+    rooms: {
+      kitchen: true,
+      bathroom: false,
+      livingroom: true,
+      bedroom: false
+    }  
+  }
+
+  switch = (e) => {
+    let roomSwitch = e.target.textContent.toLowerCase();
+    console.log('Button', roomSwitch);
+    
+    this.setState(({rooms: {kitchen, bathroom, livingroom, bedroom}}) => {
+      // console.log(this.state.rooms);
+      for ( let property in this.state.rooms) {
+        
+        if (property === roomSwitch && property === 'kitchen') {
+          console.log(property);
+          return (
+            {rooms: {
+              kitchen: !kitchen,
+              bathroom: bathroom,
+              livingroom: livingroom,
+              bedroom: bedroom
+              }
+            }
+          );
+        } else if (property === roomSwitch && property === 'bathroom') {
+          console.log(property);
+          return (
+            {rooms: {
+              kitchen: kitchen,
+              bathroom: !bathroom,
+              livingroom: livingroom,
+              bedroom: bedroom
+              }
+            }
+          );
+        }
+      }
+
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Switches kitchenAction={(e) => this.switch(e)} />
+      </div>
+    )
+  }
+
 }
 
-export default App;
+function Switches ({kitchenAction}) {
+  return (
+    <div>
+      <button onClick={kitchenAction}>Kitchen</button>
+      <button onClick={kitchenAction}>Bathroom</button>
+    </div>
+  );
+};
+
+
+export default House;
